f0e6e473c172b209f5348cad5f9db130ad8843f6
Laura Wilhelmi <laurawilhelmi.pro@gmail.com>
2023-02-13 13:52:12 +0100

    dashtables

--- Frontend/src/Dashboard/HeaderDash.jsx
+++ Frontend/src/Dashboard/HeaderDash.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import NavDashboardMobile from './NavDashboardMobile';
-
 import NavDashboardDesktop from './NavDashboardDesktop';
 
 const HeaderDash = () => {
       return (
             <div className='dashHeader'>
                   <h1>Dashboard</h1>
-                 
                   <img
-                    className="header__logo--mobile"
+                    className="dashHeader__illustration"
                     src="../public/assets/img/dashboard.png"
                     alt="Illustration dashboard"
                   />
-                
             </div>
       );
 };
