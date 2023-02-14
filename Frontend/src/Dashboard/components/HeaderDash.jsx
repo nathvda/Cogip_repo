8376b1c2b41f5f7fe2167713fb20a3b14edd8449
Laura Wilhelmi <laurawilhelmi.pro@gmail.com>
2023-02-14 14:58:39 +0100

    debut scss dash

--- Frontend/src/Dashboard/components/HeaderDash.jsx
+++ Frontend/src/Dashboard/components/HeaderDash.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const HeaderDash = () => {
-      return (
-            
+  return (
+    <div className="dashHeader">
+      <p>Dashboard</p>
 
-            <div className='dashHeader'>
-                  <h1>Dashboard</h1>
-                  <h2>Welcome back Henry!</h2>
-                  <p>You can here add an invoice, a company and some contacts</p>
-                  <img
-                    className="dashHeader__img"
-                    src="../public/assets/img/dashboard.png"
-                    alt="Illustration dashboard"
-                  />
-            </div>
-      );
+      <div className="dashHeader__Img--div">
+        <div className="dashHeader__HeyMsg">
+          <h2>Welcome back Henry!</h2>
+          <p>You can here add an invoice, a company and some contacts</p>
+        </div>
+        <img
+          className="dashHeader__Img"
+          src="../public/assets/img/dashboard.png"
+          alt="Illustration dashboard"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HeaderDash;
