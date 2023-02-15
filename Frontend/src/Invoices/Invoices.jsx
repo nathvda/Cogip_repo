54448b7ab271e996f21b33dc45543bbaec67f23a
Nour Everaert <nour.everaert.prof@gmail.com>
2023-02-15 15:31:56 +0100

    avancée design pages individuelles

--- Frontend/src/Invoices/Invoices.jsx
+++ Frontend/src/Invoices/Invoices.jsx
@@ -20,36 +20,38 @@ const Invoices = () => {
       <Header />
       <RectHeader />
       <h1 className="invoices__title">All invoices</h1>
-      <table className="invoices__table">
-        <thead>
-          <tr className="invoices__table__head">
-            <th>Invoice number</th>
-            <th>Dates due</th>
-            <th>Company</th>
-            <th>Created at</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr
-              key={"tableInvoices" + item.ref + item.id}
-              className="invoices__table__body"
-            >
-              <td>
-                <Link
-                  className="invoices__table__body--link"
-                  to={`/showinvoices/${item.id}`}
-                >
-                  {item.ref}
-                </Link>
-              </td>
-              <td>{item.date_due}</td>
-              <td>{item.id_company}</td>
-              <td>{item.created_at}</td>
+      <div className="invoices__divTable">
+        <table className="invoices__table">
+          <thead>
+            <tr className="invoices__table__head">
+              <th>Invoice number</th>
+              <th>Dates due</th>
+              <th>Company</th>
+              <th>Created at</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="invoices__table__body">
+            {data.map((item, index) => (
+              <tr
+                key={"tableInvoices" + item.ref + item.id}
+                className={index % 2 === 0 ? "even" : "odd"}
+              >
+                <td>
+                  <Link
+                    className="invoices__table__body--link"
+                    to={`/showinvoices/${item.id}`}
+                  >
+                    {item.ref}
+                  </Link>
+                </td>
+                <td>{item.date_due}</td>
+                <td>{item.id_company}</td>
+                <td>{item.created_at}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <Footer />
     </div>
   );
