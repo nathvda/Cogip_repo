c79579eb002c59810424335f2a140f0eaad81db0
Nour Everaert <nour.everaert.prof@gmail.com>
2023-02-15 12:11:22 +0100

    design homepage terminé

--- Frontend/src/HomePage/components/TableContacts.jsx
+++ Frontend/src/HomePage/components/TableContacts.jsx
@@ -25,11 +25,11 @@ const TableContacts = () => {
           </th>
         </tr>
       </thead>
-      <tbody>
-        {dataFive.map((item) => (
+      <tbody className="homepage__sectionMiddle__table--body">
+        {dataFive.map((item, index) => (
           <tr
             key={"homepageTableContacts" + item.id}
-            className="homepage__sectionMiddle__table--body"
+            className={index % 2 === 0 ? "even" : "odd"}
           >
             <td className="homepage__sectionMiddle__table--bodyCell">
               <Link
