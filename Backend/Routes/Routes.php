52e649b2d8a584654920beb1d8740ab07cd861e0
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-02-17 10:56:37 +0100

    token & api access

--- Backend/Routes/Routes.php
+++ Backend/Routes/Routes.php
@@ -15,6 +15,26 @@ use App\Validators\InvoiceValidator;
 use App\Validators\UserValidator;
 
 
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 1000');
+}
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        // may also be using PUT, PATCH, HEAD etc
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    }
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+    }
+    exit(0);
+}
+
 $router = new Router();
 
 $router->get('/', function() {
@@ -194,6 +214,8 @@ $router->mount('/login', function() use ($router){
     $router->post('/', function(){
         $payload = json_decode(file_get_contents('php://input'), true);
         $res = (new Userscontroller)->login($payload);
+        var_dump($res);
+        (new UsersController)->checkToken($res, $_ENV['SECRET']);
     });
 }
 );
