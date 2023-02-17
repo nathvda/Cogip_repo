9fefdc0011de0982a5a7c56fd09b08e15f130f51
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-02-17 12:07:06 +0100

    token cette fois ça devrait le faire

--- Backend/Routes/Routes.php
+++ Backend/Routes/Routes.php
@@ -214,8 +214,7 @@ $router->mount('/login', function() use ($router){
     $router->post('/', function(){
         $payload = json_decode(file_get_contents('php://input'), true);
         $res = (new Userscontroller)->login($payload);
-        // (new UsersController)->checkToken($res, $_ENV['SECRET']);
-        return $res;
+        echo $res;
     });
 }
 );
