656e465bb5c3b71b383d6bf0995e1db56d7654b2
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-02-20 17:52:56 +0100

    fixing header dashboard

--- Frontend/src/Dashboard/components/DashStatistics.jsx
+++ Frontend/src/Dashboard/components/DashStatistics.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const DashStatistics = () => {
-      return (
-            <h1 className="dash__sectionMiddle__title">Statistics</h1>
-      );
+  const [companies, setCompanies] = useState([]);
+  const [invoices, setInvoices] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/companies",
+      responseType: "json",
+    }).then((res) => setCompanies(res.data));
+  }, []);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/invoices",
+      responseType: "json",
+    }).then((res) => setInvoices(res.data));
+  }, []);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/contacts",
+      responseType: "json",
+    }).then((res) => setContacts(res.data));
+  }, []);
+
+  return (
+    <div className="dash__sectionMiddle">
+      <h1 className="dash__sectionMiddle__title">Statistics</h1>
+      <div className="dash_sectionMiddle__stats">
+        <div className="dash_sectionMiddle__stats--invoices">
+          <span>{invoices.length}</span>
+          invoices
+        </div>
+        <div className="dash_sectionMiddle__stats--contacts">
+          <span>{contacts.length}</span>
+          contacts
+        </div>
+        <div className="dash_sectionMiddle__stats--companies">
+          <span>{companies.length}</span>
+          companies
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashStatistics;
